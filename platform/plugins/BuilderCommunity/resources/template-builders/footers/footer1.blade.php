30e4a0f9cd71e843e762cff7ce0a513c8f06c9c0
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-09-29 10:20:38 +0700

    up code

---
+++ platform/plugins/BuilderCommunity/resources/template-builders/footers/footer1.blade.php
@@ -0,0 +1,25 @@
+<!--
+    template name:footer container
+    topic: boostrap 5,container,footer
+    category: footer
+    anthor: nguyen van hau
+    email:nguyenvanhaudev@gmail.com
+    thumbnail:
+    description:
+-->
+<div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+          <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+        <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Company, Inc</span>
+      </div>
+  
+      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+      </ul>
+    </footer>
+  </div>
