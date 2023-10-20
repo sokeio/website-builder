456365525027282c2c71f184c551a6c2295a71fa
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-10-20 16:15:27 +0700

    update

---
+++ resources/template-builders/components/page.blade.php
@@ -0,0 +1,19 @@
+<!--
+    template name:Hello components community
+    topic: boostrap 5,feature
+    category: feature
+    anthor: nguyen van hau
+    email:nguyenvanhaudev@gmail.com
+    thumbnail:
+    description:
+-->
+<style>
+    .feature-icon {
+        width: 4rem;
+        height: 4rem;
+        border-radius: 0.75rem;
+    }
+</style>
+<div class="container px-4 py-5" id="featured-3">
+    <h1>Hello components community</h1>
+</div>
